ce972ba8a6ae247eab1eaff2b79e7798e8cf2520
Kauã <gamersnes69@gmail.com>
2024-08-28 19:59:03 -0300

    first commit.

---
+++ src/classes/BubbleSort.java
@@ -0,0 +1,20 @@
+package classes;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] lista){
+        for (int i = 0; i < lista.length; i++){
+            for (int j = 0; j < lista.length - 1; j++){
+                if (lista[j] > lista[j + 1]){
+                    swap(lista, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] lista, int i, int j){
+        int aux = lista[i];
+        lista[i] = lista[j];
+        lista[j] = aux;
+    }
+}
